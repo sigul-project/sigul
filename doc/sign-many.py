6ae8e9e9b83ccb42987e45d03342c69b07c09ce7
Miloslav Trmač <mitr@redhat.com>
2011-05-23 20:32:56 +0200

    Update doc/sign-many.py for the sign-rpms command
    2011-05-23  Miloslav Trmač  <mitr@redhat.com>
    
    	* doc/sign-many.py (sign_rpm): Split from main.
    	(sign_rpms): New function.
    	(main): Demonstrate using sign_rpm or sign_rpms.

--- doc/sign-many.py
+++ doc/sign-many.py
@@ -1,24 +1,47 @@
 #! /usr/bin/env python
+import errno
 import getpass
+import os
 import subprocess
 import sys
 
-def main():
-    if len(sys.argv) < 2:
-        sys.exit('Usage: sign-many.py rpm_list other_sign-rpm_arguments\n'
-                 'The other arguments should end with key name')
-    passphrase = getpass.getpass('Key passphrase: ')
-    for rpm_id in open(sys.argv[1]):
+def sign_rpm(list_file, args, passphrase):
+    for rpm_id in open(list_file):
         rpm_id = rpm_id.rstrip('\n')
         child = subprocess.Popen(['sigul', '-v', '-v', '--batch', 'sign-rpm',
-                                  '-o', '%s.signed' % rpm_id]
-                                 + sys.argv[2:] + [rpm_id],
+                                  '-o', '%s.signed' % rpm_id] + args + [rpm_id],
                                  stdin=subprocess.PIPE)
         child.stdin.write(passphrase + '\0')
         ret = child.wait()
         if ret != 0:
             sys.exit('Exit status %d' % ret)
         print '%s done' % (rpm_id,)
+
+def sign_rpms(list_file, args, passphrase):
+    rpms = [line.rstrip('\n') for line in open(list_file)]
+    try:
+        os.mkdir('signed')
+    except OSError, e:
+        if e.errno != errno.EEXIST:
+            raise
+    child = subprocess.Popen(['sigul', '-v', '-v', '--batch', 'sign-rpms',
+                              '-o', 'signed'] + args + rpms,
+                             stdin=subprocess.PIPE)
+    child.stdin.write(passphrase + '\0')
+    ret = child.wait()
+    if ret != 0:
+        sys.exit('Exit status %d' % ret)
+    print 'All done'
+
+def main():
+    if len(sys.argv) < 2:
+        sys.exit('Usage: sign-many.py rpm_list other_sign-rpm_arguments\n'
+                 'The other arguments should end with key name')
+    passphrase = getpass.getpass('Key passphrase: ')
+    if False:
+        sign_rpm(sys.argv[1], sys.argv[2:], passphrase)
+    else:
+        sign_rpms(sys.argv[1], sys.argv[2:], passphrase)
 
 if __name__ == '__main__':
     main()
