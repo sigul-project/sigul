acb6d6768574e253ecfddc660520565bed9a02c8
Patrick Uiterwijk <patrick@puiterwijk.org>
2021-01-13 10:06:12 +0100

    Bump version number to 1.1
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- src/server_add_admin.py
+++ src/server_add_admin.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2008, 2009 Red Hat, Inc.  All rights reserved.
+# Copyright (C) 2008-2021 Red Hat, Inc.  All rights reserved.
 #
 # This copyrighted material is made available to anyone wishing to use, modify,
 # copy, or redistribute it subject to the terms and conditions of the GNU
--- src/server_gpg.py
+++ src/server_gpg.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 Red Hat, Inc.  All rights reserved.
+# Copyright (C) 2019-2021 Red Hat, Inc.  All rights reserved.
 #
 # This copyrighted material is made available to anyone wishing to use, modify,
 # copy, or redistribute it subject to the terms and conditions of the GNU
